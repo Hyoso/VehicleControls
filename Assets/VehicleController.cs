c945cfd84ddb609bd65fbd8b1c93358a9c673699
Hyoso <sam.kashing.yu@gmail.com>
2019-11-16 09:40:53 +0000

    initial commit

---
+++ Assets/VehicleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    public float m_speed = 5.0f;
+    public Rigidbody rigid;
+
+    private void Start()
+    {
+        Debug.Log(rigid.centerOfMass);
+//        rigid.centerOfMass = Vector3.down * 1.5f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.AddForce(transform.right * m_speed);
+    }
+
+    private void OnValidate()
+    {
+        if (rigid == null)
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+    }
+}
